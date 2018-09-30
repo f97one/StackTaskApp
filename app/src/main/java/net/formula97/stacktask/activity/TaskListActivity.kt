8701e9d8f121dd7310750f5b1bef1c7cc99fe114
HAJIME Fukuna <f97one@gmail.com>
2018-09-30 12:43:08 +0900

    タスクリスト画面のクラスのみとりあえず追加。 refs #73 @0.25h setProgress10 #73

---
+++ app/src/main/java/net/formula97/stacktask/activity/TaskListActivity.kt
@@ -0,0 +1,16 @@
+package net.formula97.stacktask.activity
+
+import android.os.Bundle
+import net.formula97.stacktask.R
+import kotlinx.android.synthetic.main.activity_task_list.*
+
+
+class TaskListActivity : AbstractAppActivity() {
+    override fun onCreateImpl(savedInstanceState: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun inflateLayout() {
+        setContentView(R.layout.activity_task_list)
+    }
+}
