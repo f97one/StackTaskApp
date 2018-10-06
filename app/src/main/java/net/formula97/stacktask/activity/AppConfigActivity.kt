27e825f4bbe383251190f5512edf77ee8d589708
HAJIME Fukuna <f97one@gmail.com>
2018-10-06 19:05:47 +0900

    ついでにアプリ設定画面のスケルトンも作成。 refs #73 @0.05h #74 @0.05h

---
+++ app/src/main/java/net/formula97/stacktask/activity/AppConfigActivity.kt
@@ -0,0 +1,19 @@
+package net.formula97.stacktask.activity
+
+import android.os.Bundle
+import net.formula97.stacktask.R
+
+class AppConfigActivity : AbstractAppActivity() {
+    override fun inflateLayout() {
+        setContentView(R.layout.activity_app_config)
+    }
+
+    override fun onCreateImpl(savedInstanceState: Bundle?) {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onResumeImpl() {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
--- app/src/main/java/net/formula97/stacktask/activity/TaskListActivity.kt
+++ app/src/main/java/net/formula97/stacktask/activity/TaskListActivity.kt
@@ -1,20 +1,72 @@
 package net.formula97.stacktask.activity
 
 import android.os.Bundle
-import net.formula97.stacktask.R
+import android.support.v4.view.GravityCompat
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_task_list.*
+import kotlinx.android.synthetic.main.app_navigation_drawer.*
+import net.formula97.stacktask.R
 
 
 class TaskListActivity : AbstractAppActivity() {
+
     override fun onResumeImpl() {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val uid = firebaseLogic.getCurrentUser()!!.uid
+        val itemList = firebaseLogic.readTasks(uid)
     }
 
     override fun onCreateImpl(savedInstanceState: Bundle?) {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        add_task_btn.setOnClickListener { _ ->
+            // タスク編集画面を出す
+
+        }
+
     }
 
     override fun inflateLayout() {
         setContentView(R.layout.activity_task_list)
+    }
+
+    override fun initToolBar() {
+        super.initToolBar()
+
+        supportActionBar!!.title = getString(R.string.task_list)
+        supportActionBar!!.setHomeAsUpIndicator(R.mipmap.ic_launcher)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.task_order_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item!!.itemId) {
+            android.R.id.home -> {
+                Log.d("ToolBar#onItemClick", "Homeボタンが押された")
+                app_drawer_layout.openDrawer(GravityCompat.START)
+                true
+            }
+            R.id.order_by_due_date -> {
+                // TODO 並べ替え処理を書く
+                Log.d("ToolBar#onItemClick", "期日で並べ替える")
+                true
+            }
+            R.id.order_by_priority -> {
+                // TODO 並べ替え処理を書く
+                Log.d("ToolBar#onItemClick", "優先度で並べ替える")
+                true
+            }
+            R.id.order_by_name -> {
+                // TODO 並べ替え処理を書く
+                Log.d("ToolBar#onItemClick", "名前で並べ替える")
+                true
+            }
+            else -> {
+                Log.d("ToolBar#onItemClick", "何も押されなかった")
+                false
+            }
+        }
     }
 }
