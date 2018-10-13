0bac687b1f858c75e3c36f5124ef369d8888744b
HAJIME Fukuna <f97one@gmail.com>
2018-10-13 22:37:59 +0900

    文字のない部分もクリックイベント対象とするよう、RelativeLayoutをViewHolderに追加。 refs #75 @0.1h

--- app/src/main/java/net/formula97/stacktask/view/holder/TaskListViewHolder.kt
+++ app/src/main/java/net/formula97/stacktask/view/holder/TaskListViewHolder.kt
@@ -4,11 +4,13 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.CheckBox
 import android.widget.RatingBar
+import android.widget.RelativeLayout
 import android.widget.TextView
 import net.formula97.stacktask.R
 
 class TaskListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
+    var taskItemLayout: RelativeLayout = itemView.findViewById(R.id.task_item_layout)
     var itemCompletedCheck: CheckBox = itemView.findViewById(R.id.item_completed_check)
     var itemTaskName: TextView = itemView.findViewById(R.id.item_task_name)
     var itemDueDate: TextView = itemView.findViewById(R.id.item_due_date)
